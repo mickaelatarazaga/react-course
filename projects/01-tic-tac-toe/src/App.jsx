cacd0c20600cfb444f3e26b0a849e4177161a3a0
Mickaela Tarazaga <mickaela.tarazaga@globallogic.com>
2024-05-20 12:47:41 -0300

    feat(projects/01-tic-tac-toe): Add tic-tac-toe turns and Square component

--- projects/01-tic-tac-toe/src/App.jsx
+++ projects/01-tic-tac-toe/src/App.jsx
@@ -1,13 +1,41 @@
 import { useState } from 'react'
 import './App.css'
 
+const TURNS = {
+  X: 'x',
+  O: 'o'
+};
+
+const board = Array(9).fill(null);
+
+
+const Square = ({ children, updateBoard, index }) => {
+  return (
+    <div className='square'>
+      <span>{children}</span>
+    </div>
+  )
+};
+
+
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
+    <main className='board'>
       <h1>Tic tac toe</h1>
-    </>
+      <section className='game'>
+        {
+          board.map((_, index) => {
+            return (
+              <Square key={index}>
+                {index}              
+              </Square>
+            )
+          })
+        }
+      </section>
+    </main>
   )
 }
 
