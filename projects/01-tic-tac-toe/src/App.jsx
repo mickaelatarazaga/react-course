af9bdc49cf3c567a101a7c70b4b7ab9445968a0b
Mickaela Tarazaga <mickaela.tarazaga@globallogic.com>
2024-05-20 13:05:23 -0300

    feat(projects/01-tic-tac-toe): Add logic to know the turn

--- projects/01-tic-tac-toe/src/App.jsx
+++ projects/01-tic-tac-toe/src/App.jsx
@@ -6,12 +6,12 @@ const TURNS = {
   O: 'o'
 };
 
-const board = Array(9).fill(null);
 
 
-const Square = ({ children, updateBoard, index }) => {
+const Square = ({ children, isSelected, updateBoard, index }) => {
+  const className = `square ${isSelected ? 'is-selected' : ''}`
   return (
-    <div className='square'>
+    <div className={className}>
       <span>{children}</span>
     </div>
   )
@@ -19,8 +19,8 @@ const Square = ({ children, updateBoard, index }) => {
 
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [turn, setTurn] = useState(TURNS.X);
+  const [board, setBoard] = useState(Array(9).fill('x'));
   return (
     <main className='board'>
       <h1>Tic tac toe</h1>
@@ -29,11 +29,19 @@ function App() {
           board.map((_, index) => {
             return (
               <Square key={index}>
-                {index}              
+                {board[index]}
               </Square>
             )
           })
         }
+      </section>
+      <section className='turn'>
+        <Square isSelected={turn === TURNS.X}>
+          {TURNS.X}
+        </Square>
+        <Square isSelected={turn === TURNS.O}>
+          {TURNS.O}
+        </Square>
       </section>
     </main>
   )
